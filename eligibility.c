6b46ebb1d4aebb74c13370c0a4c55be104ef0e3e
pranjalyaddav0 <25116cn246@glbitm.ac.in>
2026-02-25 12:18:16 +0530

    Create eligibility.c

---
+++ eligibility.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    
+    printf("Enter your age: ");
+    
+    scanf("%d", &age);
+
+    // Check eligibility (Age >= 18)
+    if (age >= 18) {
+        printf("You are eligible to vote.\n");
+    } else {
+        printf("You are not eligible to vote.\n");
+    }
+
+    return 0;
+}
